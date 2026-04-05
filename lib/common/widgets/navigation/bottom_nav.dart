cb789f3575796712dc0bf3079c0cc507b20018f4
IT - Nguyen Hoai Nam <nam.nguyen@heysheep.sg>
2026-04-05 13:40:59 +0700

    cmt: first commit

--- lib/common/widgets/navigation/bottom_nav.dart
+++ lib/common/widgets/navigation/bottom_nav.dart
@@ -46,6 +46,7 @@ class _BottomNavState extends State<BottomNav> {
           color: Colors.white,
           boxShadow: [
             BoxShadow(
+              // ignore: deprecated_member_use
               color: Colors.black.withOpacity(0.04),
               blurRadius: 15,
               offset: const Offset(0, -4),
